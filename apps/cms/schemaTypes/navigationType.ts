b0c1976f9217d68b7818c496d9d91f0e0c690255
Mateusz Bałdyga <matbaldyga@gmail.com>
2024-10-02 22:40:55 +0200

    feat: extend navigation type

--- apps/cms/schemaTypes/navigationType.ts
+++ apps/cms/schemaTypes/navigationType.ts
@@ -1,5 +1,16 @@
 import { defineField, defineType } from 'sanity'
 
+import type { ImageRule, StringRule, ValidationContext } from 'sanity'
+
+function subMenuValidation(rule: ImageRule | StringRule) {
+  return (rule as any).custom((value: unknown, context: ValidationContext) => {
+    if ((context.parent as any)?.subMenulayout !== 'list' && !value) {
+      return 'This field is required when submenu layout is not "list"'
+    }
+    return true
+  })
+}
+
 export const navigationType = defineType({
   name: 'navigation',
   title: 'Navigation',
@@ -22,6 +33,49 @@ export const navigationType = defineType({
               name: 'link',
               type: 'string',
               validation: rule => rule.required(),
+            }),
+
+            defineField({
+              name: 'subMenulayout',
+              title: 'Submenu Layout',
+              type: 'string',
+              initialValue: 'columns',
+              options: {
+                layout: 'radio',
+                list: [
+                  { title: 'Columns', value: 'columns' },
+                  { title: 'List', value: 'list' },
+                ],
+              },
+              validation: rule => rule.required(),
+            }),
+            defineField({
+              name: 'image',
+              title: 'Submenu Background Image',
+              type: 'image',
+              options: { hotspot: true },
+              validation: rule => rule.required(),
+              fields: [
+                defineField({
+                  name: 'alt',
+                  type: 'string',
+                  title: 'Alternative text',
+                }),
+              ],
+            }),
+            defineField({
+              name: 'subMenuHeading',
+              title: 'Submenu Heading',
+              type: 'string',
+              validation: subMenuValidation,
+              hidden: ({ parent }) => parent?.subMenulayout === 'list',
+            }),
+            defineField({
+              name: 'subMenuFooter',
+              title: 'Submenu Footer',
+              type: 'string',
+              validation: subMenuValidation,
+              hidden: ({ parent }) => parent?.subMenulayout === 'list',
             }),
             defineField({
               name: 'subItems',
